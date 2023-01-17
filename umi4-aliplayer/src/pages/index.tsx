b85c81434ae9e57271cd25fe3995b9f3ab200d36
wangxingkang <wang_xingkang@qq.com>
2023-01-17 11:11:28 +0800

    feat: 完善代码

--- umi4-aliplayer/src/pages/index.tsx
+++ umi4-aliplayer/src/pages/index.tsx
@@ -1,33 +1,54 @@
-import { useState } from 'react';
-import { Modal, Button } from 'antd';
+import { Modal, Button, Card } from 'antd';
+import { useModal } from '@pansy/react-hooks';
 import Player from '@pansy/react-aliplayer';
 
-import { ButtonProps } from 'antd/es/button';
+import type { FC } from 'react';
+import type { UseModalResult } from '@pansy/react-hooks';
 
-export default () => {
-  const [open, setOpen] = useState(false);
-
-  const handleClick: ButtonProps['onClick'] = () => {
-    setOpen(true);
-  }
+const PlayerModal: FC<{ modal: UseModalResult<string> }> = (props) => {
+  const { modal } = props;
 
   return (
-    <>
-      <Button onClick={handleClick}>Open Modal</Button>
-      <Modal
-        title="预览视频"
-        centered
-        footer={null}
-        open={open}
-        width={1080}
-        onCancel={() => setOpen(false)}
-      >
-        <div style={{ width: 780, height: 585 }}>
-          <Player
-            source="//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4"
-          />
-        </div>
-      </Modal>
-    </>
+    <Modal
+      title="预览视频"
+      centered
+      footer={null}
+      open={modal.visible}
+      width={1080}
+      onCancel={modal.close}
+    >
+      <div style={{ width: 780, height: 585 }}>
+        <Player
+          source={modal.data}
+        />
+      </div>
+    </Modal>
   )
+
+}
+
+const sources = [
+  '//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4',
+  '//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4',
+  '//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4',
+];
+
+export default () => {
+  const modal = useModal<string>();
+
+  const handleClick = (source: string) => {
+    modal.open(source);
+  }
+
+  return sources.map((source, key) => {
+    return (
+      <>
+        <Card title={`标题${key + 1}`} key={key}>
+          <Button onClick={() => { handleClick(source) }}>Open Modal</Button>
+        </Card>
+
+        <PlayerModal modal={modal} />
+      </>
+    )
+  })
 }
