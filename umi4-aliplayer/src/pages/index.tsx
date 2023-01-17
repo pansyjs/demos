8909d3a4134c3d2b1f65df5febd37b01d0c8dcf0
wangxingkang <wang_xingkang@qq.com>
2023-01-17 21:12:51 +0800

    feat: 添加分页示例

--- umi4-aliplayer/src/pages/index.tsx
+++ umi4-aliplayer/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Modal, Button, Card } from 'antd';
+import { useState } from 'react'
+import { Modal, Button, Card, Pagination } from 'antd';
 import { useModal } from '@pansy/react-hooks';
 import Player from '@pansy/react-aliplayer';
 
@@ -14,33 +15,41 @@ const PlayerModal: FC<{ modal: UseModalResult<string> }> = (props) => {
       centered
       footer={null}
       open={modal.visible}
-      width={1080}
+      width={780}
       onCancel={modal.close}
     >
-      <div style={{ width: 780, height: 585 }}>
-        <Player
-          source={modal.data}
-        />
-      </div>
+      <Player
+        source={modal.data}
+        style={{ height: 400 }}
+        options={{
+          autoplay: true,
+        }}
+      />
     </Modal>
   )
 
 }
 
-const sources = [
-  '//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4',
+const sources1 = [
   '//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4',
   '//stream7.iqilu.com/10339/upload_transcode/202002/18/202002181038474liyNnnSzz.mp4',
 ];
 
+const sources2 = [
+  '//stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4',
+  '//stream7.iqilu.com/10339/upload_transcode/202002/18/20200218114723HDu3hhxqIT.mp4',
+];
+
 export default () => {
   const modal = useModal<string>();
+  const [sourceList, setSourceList] = useState(sources1)
+  const [current, setCurrent] = useState(1)
 
   const handleClick = (source: string) => {
     modal.open(source);
   }
 
-  const players = sources.map((source, key) => {
+  const players = sourceList.map((source, key) => {
     return (
       <Card title={`标题${key + 1}`} key={key}>
         <Button onClick={() => { handleClick(source) }}>Open Modal</Button>
@@ -52,6 +61,16 @@ export default () => {
     <>
       {players}
       <PlayerModal modal={modal} />
+      <Pagination
+        total={4}
+        defaultPageSize={2}
+        current={current}
+        style={{marginLeft: "auto"}}
+        onChange={(page) => {
+          setCurrent(page);
+          setSourceList(page === 1 ? sources1 : sources2);
+        }}
+      />
     </>
   )
 }
